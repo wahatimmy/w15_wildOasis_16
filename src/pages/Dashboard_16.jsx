97d799b83d1d084c4b27afe2f2f69eb3654099a5
wahatimmy <211418016@365.tku.edu.tw>
2024-06-12 20:23:33 +0800

    descrip

--- src/pages/Dashboard_16.jsx
+++ src/pages/Dashboard_16.jsx
@@ -1,5 +1,13 @@
 const Dashboard_16 = () => {
-  return <>Dashboard_16 page</>;
+  return (
+    <>
+      <span>Final-2 Exam of Web Prog, 2024</span>
+      <span>Guest name: Lai Yu Ting</span>
+      <span>Guest student ID: 211418016</span>
+      <span>Supabase email: 211418016@gmail.com</span>
+      <span>Supabase userID: wahatimmy</span>
+    </>
+  );
 };
 
 export default Dashboard_16;
